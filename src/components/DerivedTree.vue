7b8a19a45372c0e46feb9108f4a8ac48f69cb840
Matsuzaki Rei <matsuzaki.rei@gmail.com>
2020-01-19 21:49:38 +0900

    change rem to ch

--- src/components/DerivedTree.vue
+++ src/components/DerivedTree.vue
@@ -2,20 +2,20 @@
   <svg :width="width + 'rem'" :height="height + 'rem'" style="font-size: 20px">
     <svg v-for="item of items" :key="item.id.value">
       <line 
-        :x1="item.view.x1()+'rem'" 
-        :x2="item.view.x2()+'rem'"
+        :x1="item.view.x1()+'ch'" 
+        :x2="item.view.x2()+'ch'"
         :y1="item.view.y+'rem'"
         :y2="item.view.y+'rem'"
         stroke="black">
       </line>
       <text 
-        :x="item.view.x2()+'rem'" 
-        :y="item.view.y+'rem'"
-        dominant-baseline="central">
-        {{ item.rule.name }}
+            :x="item.view.x2()+'ch'" 
+            :y="item.view.y+'rem'"
+            dominant-baseline="central">
+          {{ item.rule.name }}
       </text>
       <text 
-        :x="item.view.x+'rem'" 
+        :x="item.view.cx()+'ch'" 
         :y="item.view.y+'rem'"
         text-anchor="middle"
         dominant-baseline="hanging">
@@ -35,25 +35,35 @@ import rule from './Rule.vue'
 class RuleView {
   id: ruleModel.Id
   x: number
-  width: number
+  lineWidth: number
+  nameWidth: number
   y: number
   static empty(id: ruleModel.Id): RuleView {
-    return new RuleView(id, -1, -1 ,-1)
+    return new RuleView(id, -1, -1 ,-1, -1)
     }
-  constructor(id: ruleModel.Id, x: number, width: number, y: number) {
+  constructor(id: ruleModel.Id, x: number, y: number, 
+    lineWidth: number, nameWidth: number) {
     this.id = id
     this.x = x
-    this.width = width
     this.y = y
+    this.lineWidth = lineWidth
+    this.nameWidth = nameWidth
+    
   }
   x1(): number {
-    return this.x - this.width / 2
+    return this.x
   }
   x2(): number {
-    return this.x + this.width / 2
+    return this.x + this.lineWidth
+  }
+  cx(): number {
+    return this.x + this.lineWidth / 2
+  }
+  width(): number {
+    return this.lineWidth + this.nameWidth
   }
   toString(): string {
-    return `id: ${this.id.value}, {x: ${this.x}, y: ${this.y}, w: ${this.width}}`
+    return `id: ${this.id.value}, {x: ${this.x}, y: ${this.y}`
   }
 }
 interface Item {
@@ -70,6 +80,7 @@ interface Item {
 export default class DerivedTree extends Vue {
   height: number = 20
   width: number = 50
+  // ruleNameWidth: number = 5
   items: Item[] = []
   topItemId: ruleModel.Id | null = null
 
@@ -79,13 +90,13 @@ export default class DerivedTree extends Vue {
   getView(id: ruleModel.Id): RuleView {
     return this.items.find(v => v.id.value == id.value)!.view
   }
-  buildRules() {
+  initItems() {
     console.log('refresh item')
 
     const idGen = new ruleModel.IdGenerator()
 
     const rule11 = new ruleModel.TextRule(idGen.newId(), 'EIfTrue', 0)
-    rule11.setRelation(new relation.TextRelation('If true then false false'))
+    rule11.setRelation(new relation.TextRelation('if true then false else false'))
 
     const rule12 = new ruleModel.TextRule(idGen.newId(), 'RULE-1-2', 0)
     rule12.setRelation(new relation.TextRelation('rel2'))
@@ -100,62 +111,6 @@ export default class DerivedTree extends Vue {
     rule3.setRelation(new relation.TextRelation('bbb'))
 
     const rules = [rule11, rule12, rule2, rule3]
-    return rules
-  }
-  refreshRuleView(id: ruleModel.Id): void {
-    console.log('refresh view', id.value)
-    if(!this.topItemId) return
-    const rule = this.getRule(id)
-    const view = this.getView(id)
-
-    const ws: number[] = []
-    for(let parentId of rule.parentIds) {
-      const parentRule = this.getRule(parentId!)
-      const w = parentRule.relation!.toString().length + parentRule.name.toString().length
-      ws.push(w)
-    }
-
-    const widthFromParent = ws.reduceRight((x, y) => x+y, 0)
-    const widthFromRelation = rule.relation!.toString().length
-    view.width = Math.max(widthFromParent, widthFromRelation)
-
-    let x1 = view.x - view.width / 2
-    for(let i = 0; i < rule.parentIds.length; i++) {
-      const parentId = rule.parentIds[i]!
-      const w = ws[i]
-      const parentRule = this.getRule(parentId)
-      const parentView = this.getView(parentId)
-
-      parentView.x = x1 + w / 2
-      parentView.y = view.y - 1
-
-      console.log(id.value, parentId.value)
-
-      this.refreshRuleView(parentId)
-
-      x1 += w
-    }
-  }
-  refreshViews() {
-    console.log('refresh views')
-    if(!this.topItemId) return
-
-    const topItem = this.getRule(this.topItemId)
-
-    const topView = this.getView(this.topItemId)
-    topView.x = this.width / 2
-    topView.y = this.height - 5
-    this.refreshRuleView(topView.id)
-  }
-
-  private async refresh() {
-    console.log("refresh")
-    // this.refreshItems()
-    this.refreshViews()
-  }
-
-  created() {
-    const rules = this.buildRules()
     const items: Item[] = rules.map(v => {
       const item: Item = {
         id: v.id,
@@ -166,8 +121,75 @@ export default class DerivedTree extends Vue {
     })
 
     this.items.splice(0, this.items.length, ...items)
-    console.log('this.items.length', this.items.length)
-    this.topItemId = this.items[this.items.length-1].id
+    this.topItemId = rule3.id
+  }
+  refreshViewsWidth(id: ruleModel.Id): number {
+    const rule = this.getRule(id)
+    const view = this.getView(id)
+
+    let pWidth = 0
+    for (let i = 0; i < rule.parentIds.length; i++) {
+      const pId = rule.parentIds[i]
+      if (pId) {
+        this.refreshViewsWidth(pId)
+        const pView = this.getView(pId)
+        pWidth += pView.lineWidth
+        if (i < rule.parentIds.length - 1 ) {
+          pWidth += pView.nameWidth
+        }
+      }
+    }
+
+    let relationWidth = 3
+    if(rule.relation) {
+      relationWidth = rule.relation.toString().length
+    }
+
+    view.lineWidth = Math.max(pWidth, relationWidth)
+    view.nameWidth = rule.name.length
+
+    console.log(id.value, view.lineWidth, view.nameWidth)
+
+    return view.width()
+  }
+  refreshViewsXY(id: ruleModel.Id, x: number, y: number): void {
+    const rule = this.getRule(id)
+    const view = this.getView(id)
+
+    let xx = x
+    for(let pId of rule.parentIds) {
+      if(pId) {
+        const pRule = this.getRule(pId)
+        const pView = this.getView(pId)
+        this.refreshViewsXY(pId, xx, y-1)
+        xx += pView.width()
+      }      
+    }
+
+    view.x = x
+    view.y = y
+  }
+  refreshViews() {
+    console.log('refresh views')
+    if(!this.topItemId) return
+
+    // const topItem = this.getRule(this.topItemId)
+
+    // const topView = this.getView(this.topItemId)
+    // topView.x = this.width / 2
+    // topView.y = this.height - 5
+    this.refreshViewsWidth(this.topItemId)
+    this.refreshViewsXY(this.topItemId, 2, this.height - 5)
+  }
+
+  private async refresh() {
+    console.log("refresh")
+    // this.refreshItems()
+    this.refreshViews()
+  }
+
+  created() {
+    this.initItems()
     this.refresh()
 
     console.log(this.items)
